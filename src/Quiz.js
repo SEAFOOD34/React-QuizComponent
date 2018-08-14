c495e181313d04269d54b0d375867f971c323325
Seafood34 <sonicfiredasher2@gmail.com>
2018-08-13 21:28:28 -0700

    Completed the module5

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,9 @@ class Quiz extends Component {
     this.state = {quiz_position: 1}
   }
   showNextQuestion() {
-    this.setState = {quiz_position: quiz_position + 1}
+    this.setState((state) => {
+      return {quiz_position: state.quiz_position + 1};
+    });
   }
   render() {
     const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length);
